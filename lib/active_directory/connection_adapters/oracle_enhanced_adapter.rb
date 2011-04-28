fc4453cc61647708da14d7339b54c3fc44959950
Steve Lamotte <slamotte@winnipeg.ca>
2011-04-28 15:07:43 -0500

    Added some features

--- lib/active_directory/connection_adapters/oracle_enhanced_adapter.rb
+++ lib/active_directory/connection_adapters/oracle_enhanced_adapter.rb
@@ -1,37 +1,56 @@
 module ActiveRecord
   module ConnectionAdapters
-    module OracleEnhancedAdapter
+    class OracleEnhancedAdapter
+
+      # Set our new enhanced defaults
+      set_enhanced_defaults
 
       # Maximum length of an Oracle VARCHAR2 field
       VARCHAR2_MAX_LENGTH = 4000
 
       # Indicate whether to use varchar2(length) for TEXT columns or clob (default).
       # Pass true or false to select varchar2 or clob, respectively. Optionally pass in the desired varchar2 length.
-      def set_text_storage_representation(use_varchar2, length = VARCHAR2_MAX_LENGTH)
+      def self.set_text_storage_representation(use_varchar2, length = VARCHAR2_MAX_LENGTH)
         NATIVE_DATABASE_TYPES[:text] = use_varchar2 ? {:name => 'VARCHAR2', :limit => length} : {:name => 'CLOB'}
       end
 
-      # Create helper methods to check what storage representation is in effect
+      # Create convenience methods to check what storage representation is in effect
       # e.g. text_is_clob?, text_is_varchar2?
       %w(clob varchar2).each do |format|
-        define_method "text_is_#{format}?" do
-          NATIVE_DATABASE_TYPES[:text][:name].downcase == format
+        (class << self; self; end).instance_eval do
+          define_method "text_is_#{format}?" do
+            NATIVE_DATABASE_TYPES[:text][:name].downcase == format
+          end
         end
       end
 
-      # Convert an existing CLOB field into a VARCHAR2
+      # Convert an existing CLOB field into a VARCHAR2. For use in migrations.
       def convert_clob_to_varchar2(table_name, column_name, varchar2_length = VARCHAR2_MAX_LENGTH, temp_column_name = nil)
         convert_text_column_storage_type table_name, column_name, "varchar2#{varchar2_length}", temp_column_name
       end
 
-      # Convert an existing VARCHAR2 field into a CLOB
+      # Convert an existing VARCHAR2 field into a CLOB. For use in migrations.
       def convert_varchar2_to_clob(table_name, column_name, temp_column_name = nil)
         convert_text_column_storage_type table_name, column_name, 'clob', temp_column_name
       end
 
       private
 
+      # Enable our enhanced settings/defaults
+      def set_enhanced_defaults
+        # Use varchar2(4000) for TEXT fields
+        set_text_storage_representation true
+
+        # Cache columns because we don't need to hit the database to check metadata all the time
+        self.cache_columns = true
+
+        # Oracle's default sequence behavious doesn't appeal to us OCD freaks who prefer monotonically increasing sequences
+        self.default_sequence_start_value = "1 NOCACHE ORDER"
+      end
+
+      # Convert a field from varchar2 to clob or vice versa
       def convert_text_column_storage_type(table_name, column_name, to_type, temp_column_name)
+        # Inspired by http://asktom.oracle.com/pls/asktom/f?p=100:11:0::::P11_QUESTION_ID:1770086700346491686
         temp_col = temp_column_name || 'oee_temp_col'
         cmds = <<-SQL
           alter table #{table_name} add #{temp_col} #{to_type}
@@ -42,9 +61,9 @@ module ActiveRecord
           update #{table_name} set #{column_name} = #{temp_col}
           alter table #{table_name} drop column #{temp_col}
         SQL
-        cmds.split('\n').each{|cmd| @connection.execute cmd}
+        cmds.split('\n').each{|cmd| execute cmd}
       end
-
     end
+
   end
 end
--- oracle-enhanced-enhanced.gemspec
+++ oracle-enhanced-enhanced.gemspec
@@ -8,9 +8,9 @@ Gem::Specification.new do |s|
   s.platform    = Gem::Platform::RUBY
   s.authors     = ["Steve Lamotte"]
   s.email       = ["slamotte@winnipeg.ca"]
-  s.homepage    = ""
+  s.homepage    = "http://gems.winnipegtransit.org"
   s.summary     = %q{Additional enhancements/customizations to the Oracle Enhanced adapter}
-  s.description = %q{This gem includes several enhancements and customizationt to the standard Oracle Enhanced adapter, which is still required. Any customizations to this adapter's behaviour or additional Oracle-specific utilities should be added here.}
+  s.description = %q{This gem includes several enhancements and customizations to the standard Oracle Enhanced adapter, which is still required. Any customizations to this adapter's behaviour or additional Oracle-specific utilities should be added here.}
 
   s.rubyforge_project = "oracle-enhanced-enhanced"
 
@@ -19,5 +19,5 @@ Gem::Specification.new do |s|
   s.executables   = `git ls-files -- bin/*`.split("\n").map{ |f| File.basename(f) }
   s.require_paths = ["lib"]
 
-  s.add_dependency "activerecord-oracle_enhanced-adapter"
+  s.add_runtime_dependency "activerecord-oracle_enhanced-adapter"
 end
